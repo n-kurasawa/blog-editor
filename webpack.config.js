113d2cde61ee34037ecf818a221d483d7ed6d997
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2017-12-30 23:08:30 +0900

    add build

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: './dist/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-react-jsx'],
+        },
+      },
+    ],
+  },
+};
