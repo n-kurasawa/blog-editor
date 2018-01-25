e61d0c6f0ace99ea9c1038b7dc0f348cf929eb4f
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-01-25 20:55:14 +0900

    refactor Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withStyles } from 'material-ui/styles';
-import { AppBar, Toolbar, Icon, IconButton } from 'material-ui';
+import { Icon, IconButton } from 'material-ui';
 import { upload } from '../reducers/remoteArticle';
+import styles from './Header.css';
 
-const drawerWidth = 240;
+const Header = connect(state => state, { upload })(({ upload, editor }) => (
+  <React.Fragment>
+    <header className={styles.appBar}>
+      <IconButton
+        className={styles.btn}
+        onClick={() => {
+          upload(editor);
+        }}
+      >
+        <Icon color="action">file_upload</Icon>
+      </IconButton>
+    </header>
+  </React.Fragment>
+));
 
-const styles = theme => ({
-  appBar: {
-    minHeight: 0,
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    position: 'absolute',
-    backgroundColor: '#fafafa',
-    boxShadow: '0 0 0 0',
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-  },
-  toolbar: {
-    minHeight: 0,
-  },
-  flex: {
-    flex: 1,
-  },
-});
-
-const Header = connect(state => state, { upload })(
-  ({ classes, upload, editor }) => (
-    <React.Fragment>
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar className={classes.toolbar}>
-          <div className={classes.flex} />
-          <IconButton
-            onClick={() => {
-              upload(editor);
-            }}
-          >
-            <Icon color="action">file_upload</Icon>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  ),
-);
-
-export default withStyles(styles)(Header);
+export default Header;
