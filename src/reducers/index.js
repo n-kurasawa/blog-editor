ee423f6c0921ca016d3327c6d74770c82f437f4b
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-02-11 09:53:47 +0900

    remote storage reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from 'redux';
 import article from './article';
 import editor from './editor';
 import remoteArticle from './remoteArticle';
-import storage from './storage';
 
 export default combineReducers({
   article,
   editor,
   remoteArticle,
-  storage,
 });
